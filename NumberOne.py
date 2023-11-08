3871457a882c6bafa20cb4212714f17299ee2f19
exxxpm <127376127+exxxpm@users.noreply.github.com>
2023-11-08 08:32:16 +0400

    Added the first script

--- NumberOne.py
+++ NumberOne.py
@@ -1,1 +1,14 @@
-print('numberone')
+import os
+import csv
+
+def create_csv_annotation(class_name: str, annotation_filename: str) -> None:
+    """Генерирует CSV-аннотацию на основе класса и имени файла."""
+    dataset_path = os.path.join('dataset')
+    class_names = os.listdir(dataset_path)
+    with open(annotation_filename, mode="w", newline='') as file:
+        file_writer = csv.writer(file, delimiter=",")
+        for name in class_names:
+            file_writer.writerow([os.path.abspath(name), os.path.join(dataset_path, name), class_name])
+
+def run_annotation_generation(class_name: str, annotation_filename: str) -> None:
+    create_csv_annotation(class_name, annotation_filename)
