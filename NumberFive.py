77e13958e298d582c944cda6e6084f12ceb5fba6
exxxpm <127376127+exxxpm@users.noreply.github.com>
2023-11-08 09:04:38 +0400

    Added the fifth script

--- NumberFive.py
+++ NumberFive.py
@@ -1,1 +1,16 @@
-print('numberfive')
+import os
+
+class IteratorTask:
+    def __init__(self, path: str):
+        """Инициализирует поля класса."""
+        self.file_names = os.listdir(os.path.join('dataset', path))
+        self.counter = 0
+        self.limit = len(self.file_names)
+        self.path = path
+
+    def __next__(self):
+        if self.counter < self.limit:
+            self.counter += 1
+            return os.path.join(self.path, self.file_names[self.counter - 1])
+        else:
+            raise StopIteration
