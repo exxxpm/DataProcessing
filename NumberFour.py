3d3643786ebb2a9ebfedc9e30f4d723f76a2e6eb
exxxpm <127376127+exxxpm@users.noreply.github.com>
2023-11-08 09:00:09 +0400

    Added a fourth script

--- NumberFour.py
+++ NumberFour.py
@@ -1,1 +1,10 @@
-print('numberfour')
+import os
+from typing import Optional
+
+def get_next_element(class_mark: str) -> Optional[str]:
+    """Постепенно возвращает элементы класса."""
+    path = os.path.join("dataset", class_mark)
+    names_list = os.listdir(path)
+    names_list.append(None)
+    for i in range(0, len(names_list)):
+        yield names_list[i]
